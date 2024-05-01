91b4d1f54863efbd2800e6c898e06684ac57e942
goosetea04 <gusti.fatu@gmail.com>
2024-05-01 14:36:24 +0700

    Transaction Service Implementation

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -12,6 +12,9 @@ pub mod services {
 #[derive(Default)]
 pub struct MyPaymentService {}
 
+#[serive(Default)]
+pub struct MyTransactionService{}
+
 #[tonic::async_trait]
 impl PaymentService for MyPaymentService{
     async fn process_payment(
@@ -22,13 +25,45 @@ impl PaymentService for MyPaymentService{
     }
 }
 
+#[tonic::async_trait]
+impl TransactionService for MyTransactionService {
+    type GetTransactionHistoryStream = ReceiverStream<Result<TransactionResponse, Status>>;
+
+    async fn get_transaction_history{
+        &self,
+        request: Request<TransactionRequest>.
+    } -> Result<Response<Self::GetTransactionHistoryStream>, Status> {
+        println!("Received transaction history request: {}:?", request);
+        let (tx, rx): (Sender<Result<TransactionResponse, Status>>, Receiver<Result<TransactionResponse, Status>>) = mpsc::channel(4);
+
+        tokio::spawn(async move {
+            for i in 0..30 {
+                if tx.send(Ok(TransactionResponse{
+                    transaction_id: format!("trans_{}", i),
+                    status: "Completed".to_string(),
+                    amount:100.0,
+                    timestamp: "2022-01-01T12:00:00Z".to_string(),
+                })).await.is_err() {
+                    breakl
+                }
+                if i%10 == 9 {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            }
+        });
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
 #[tokio:main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let payment_service = MyPaymentService::default();
+    let transaction_service = MyTransactionService::default();
 
     Server::builder()
         .add_service(PaymentServiceServer::new(payment_service))
+        .add_service(TransactionServiceServer::new(transaction_service))
         .serve(addr)
         .await?;
     Ok(())
